507d5cf692bd6981611ba28e6c736a879ac1ef43
yunchoun <chdns2000@naver.com>
2023-07-02 10:19:45 +0900

    07-02

---
+++ js/833.8dea8495.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcomprj01"]=self["webpackChunkcomprj01"]||[]).push([[833],{833:function(n,r,u){u.r(r),u.d(r,{default:function(){return l}});var t=u(3396);const a={class:"about"},c=(0,t._)("h1",null,"This is an about page",-1),e=[c];function s(n,r){return(0,t.wg)(),(0,t.iD)("div",a,e)}var o=u(89);const i={},f=(0,o.Z)(i,[["render",s]]);var l=f}}]);
+//# sourceMappingURL=833.8dea8495.js.map
